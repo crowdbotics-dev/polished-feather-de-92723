05bed171573f07ed44c4f691d5e5628f380cdd2f
Crowdbotics <engineering@crowdbotics.com>
2023-06-19 00:51:51 +0000

    Initial commit.

---
+++ backend/polished_feather_de_92723/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for polished_feather_de_92723 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polished_feather_de_92723.settings')
+
+application = get_wsgi_application()
